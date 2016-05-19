2e135bfaacfffa1d3c18f0357199eb82984a474e
iamyangchen <yangchen08@tamu.edu>
2016-05-19 07:51:12 -0500

    initial commit for openwrt libs

---
+++ openwrt/buildroot.sh
@@ -0,0 +1,10 @@
+# svn export svn://svn.openwrt.org/openwrt/tags/backfire_10.03
+
+# backfire 3.1rc5
+#svn export svn://svn.openwrt.org/openwrt/branches/backfire/@27608
+
+#rc6
+# svn export svn://svn.openwrt.org/openwrt/branches/backfire/@28680
+
+#10.03.1
+svn export svn://svn.openwrt.org/openwrt/tags/backfire_10.03.1
